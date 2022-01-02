574320fe7d215e17ea4eeb8e5e8b553781246238
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-01-02 16:28:50 -0500

    update

--- src/scanner.rs
+++ src/scanner.rs
@@ -30,7 +30,7 @@ impl Scanner {
             self.start = self.current;
             self.scan_token()?;
         }
-        let token = Token::new(TokenType::Eof, "".to_string(), self.line)?;
+        let token = Token::new(TokenType::Eof, "".to_string(), None, self.line)?;
         self.tokens.push(token);
         Ok(())
     }
@@ -45,43 +45,45 @@ impl Scanner {
     fn scan_token(&mut self) -> Result<(), RloxError> {
         let c: char = self.advance();
         match c {
-            '(' => self.add_token(TokenType::LeftParen),
-            ')' => self.add_token(TokenType::RightParen),
-            '{' => self.add_token(TokenType::LeftBrace),
-            '}' => self.add_token(TokenType::RightBrace),
-            ',' => self.add_token(TokenType::Comma),
-            '.' => self.add_token(TokenType::Dot),
-            '-' => self.add_token(TokenType::Minus),
-            '+' => self.add_token(TokenType::Plus),
-            ';' => self.add_token(TokenType::Semicolon),
-            '*' => self.add_token(TokenType::Star),
+            '(' => self.add_token(TokenType::LeftParen, None),
+            ')' => self.add_token(TokenType::RightParen, None),
+            '{' => self.add_token(TokenType::LeftBrace, None),
+            '}' => self.add_token(TokenType::RightBrace, None),
+            ',' => self.add_token(TokenType::Comma, None),
+            '.' => self.add_token(TokenType::Dot, None),
+            '-' => self.add_token(TokenType::Minus, None),
+            '+' => self.add_token(TokenType::Plus, None),
+            ';' => self.add_token(TokenType::Semicolon, None),
+            '*' => self.add_token(TokenType::Star, None),
             '!' => match self.advance_if_match('=') {
-                true => self.add_token(TokenType::BangEqual),
-                false => self.add_token(TokenType::Equal),
+                true => self.add_token(TokenType::BangEqual, None),
+                false => self.add_token(TokenType::Equal, None),
             },
             '=' => match self.advance_if_match('=') {
-                true => self.add_token(TokenType::EqualEqual),
-                false => self.add_token(TokenType::Equal),
+                true => self.add_token(TokenType::EqualEqual, None),
+                false => self.add_token(TokenType::Equal, None),
             },
             '<' => match self.advance_if_match('=') {
-                true => self.add_token(TokenType::LessEqual),
-                false => self.add_token(TokenType::Less),
+                true => self.add_token(TokenType::LessEqual, None),
+                false => self.add_token(TokenType::Less, None),
             },
             '>' => match self.advance_if_match('=') {
-                true => self.add_token(TokenType::GreaterEqual),
-                false => self.add_token(TokenType::Greater),
+                true => self.add_token(TokenType::GreaterEqual, None),
+                false => self.add_token(TokenType::Greater, None),
             },
             '/' => match self.advance_if_match('/') {
                 true => {
-                    while self.peek() != '\n' && !self.is_at_end() {
-                        self.advance();
-                    }
+                    self.advance_through_end_of_line();
                     Ok(())
-                },
-                false => {
-                    self.add_token(TokenType::Slash)
                 }
+                false => self.add_token(TokenType::Slash, None),
             },
+            ' ' | '\r' | '\t' => Ok(()),
+            '\n' => {
+                self.line += 1;
+                Ok(())
+            }
+            '"' => todo!(),
             _ => Err(RloxError::SyntaxError(RloxSyntaxError {
                 line_number: self.line,
                 description: "Unexpected character.".to_string(),
@@ -113,11 +115,25 @@ impl Scanner {
         return true;
     }
 
-    fn add_token(&mut self, token_type: TokenType) -> Result<(), RloxError> {
+    fn add_token(
+        &mut self,
+        token_type: TokenType,
+        literal: Option<String>,
+    ) -> Result<(), RloxError> {
         let text = &self.source[self.start..self.current];
-        self.tokens
-            .push(Token::new(token_type, text.to_string(), self.line)?);
+        self.tokens.push(Token::new(
+            token_type,
+            text.to_string(),
+            literal,
+            self.line,
+        )?);
         Ok(())
+    }
+
+    fn advance_through_end_of_line(&mut self) {
+        while self.peek() != '\n' && !self.is_at_end() {
+            self.advance();
+        }
     }
 
     fn peek(&mut self) -> char {
@@ -125,6 +141,25 @@ impl Scanner {
             true => '\0',
             false => self.source.chars().nth(self.current).unwrap(),
         }
+    }
+
+    fn consume_string(&mut self) -> Result<(), RloxError> {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+        if self.is_at_end() {
+            return Err(RloxError::SyntaxError(RloxSyntaxError {
+                line_number: self.line,
+                description: "Unterminated string.".to_string(),
+            }));
+        }
+        self.advance();
+        let value = &self.source[&self.start + 1..&self.current - 1];
+        self.add_token(TokenType::String, Some(value.to_string()))?;
+        Ok(())
     }
 }
 
--- src/token.rs
+++ src/token.rs
@@ -4,10 +4,10 @@ use crate::error::{RloxError, RloxSyntaxError};
 
 #[derive(Debug)]
 pub struct Token {
-    pub token_type: TokenType,
-    pub lexeme: String,
-    //literal: there is no Object type in Rust <--- TODO: handle this
-    pub line_number: usize,
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<String>, // TODO
+    line_number: usize,
 }
 
 impl Display for Token {
@@ -20,11 +20,13 @@ impl Token {
     pub fn new(
         token_type: TokenType,
         lexeme: String,
+        literal: Option<String>,
         line_number: usize,
     ) -> Result<Self, RloxError> {
         Ok(Token {
             token_type,
             lexeme,
+            literal,
             line_number,
         })
     }
